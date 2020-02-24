bc868d30e718b55b14d42f80dfb715a8faea5433
Moritz Löchner <momo.loechner@gmail.com>
2020-02-24 17:27:58 +0100

    reconfigured the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :new, :create] do
-    resources :paintings
-    resources :bookings, only: [:index, :show, :destroy]
-  end
+  get '/dashboard', to: 'pages#dashboard', as: :dashboard
+  resources :paintings
+  resources :bookings, only: [:index, :show, :destroy]
+
   resources :paintings, only: [:index, :show] do
     resources :bookings, only: [:new, :create]
   end
