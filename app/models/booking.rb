b9d79c8d7a2419b5425ed2dbbb14398a8f0cf12d
Max Wolff <maxwolff1103@gmail.com>
2020-02-24 14:25:33 +0100

    Validations added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :painting
   belongs_to :user
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates_uniqueness_of :painting_id, scope: [:user_id]
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,3 +1,8 @@
 class Painting < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  validates :title, presence: true
+  validates :artist, presence: true
+  validates :price_per_day, presence: true
+  validates :address, presence: true
 end
