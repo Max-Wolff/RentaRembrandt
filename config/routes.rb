18234a098eee97d73b7609466a8da0cdad1647e9
Moritz Löchner <momo.loechner@gmail.com>
2020-02-24 18:02:12 +0100

    added the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # rout for current user
   get '/dashboard', to: 'pages#dashboard', as: :dashboard
+
   resources :paintings
   resources :bookings, only: [:index, :show, :destroy]
 
