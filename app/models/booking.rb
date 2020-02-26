9746f31910885dd0bcc1f170c4ee227fe2b4ef6c
Moritz Löchner <momo.loechner@gmail.com>
2020-02-26 16:02:07 +0100

    added the booking form on show page

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -14,6 +14,10 @@ class Booking < ApplicationRecord
   def end_date_after_start_date
     return if end_date.blank? || start_date.blank?
 
+    if start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+
     if end_date < start_date
       errors.add(:end_date, "must be after the start date")
     end
