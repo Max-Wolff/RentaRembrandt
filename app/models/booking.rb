1133aab4028735ce3d408122008f18abdd7f7966
Moritz Löchner <momo.loechner@gmail.com>
2020-02-26 13:05:30 +0100

    booking validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,21 @@
+require_relative '../validators/availability_validator'
+
 class Booking < ApplicationRecord
   belongs_to :painting
   belongs_to :user
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validates :start_date, :end_date, presence: true, availability: true
+  validate :end_date_after_start_date
   validates_uniqueness_of :painting_id, scope: [:user_id]
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+ end
 end
