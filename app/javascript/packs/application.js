f191135f940d0a8d06f3b8054ec0d13d87ac419f
Max Wolff <maxwolff1103@gmail.com>
2020-02-26 12:32:48 +0100

    Map created

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,17 @@
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import { updateNavbarOnScroll } from '../components/navbar';
 import { bannerCarousel } from '../components/banner';
 import { changeActiveTab } from '../components/dashboard';
+import { initMapbox } from '../plugins/init_mapbox';
+
 
 updateNavbarOnScroll();
 
 bannerCarousel();
 
 changeActiveTab();
+
+initMapbox();
