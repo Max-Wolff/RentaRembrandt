fcf5e344e57b71de1c3cbfb362766dbd1bc92b7f
Moritz Löchner <momo.loechner@gmail.com>
2020-02-28 10:47:46 +0100

    bookmark controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,8 +1,18 @@
 class BookmarksController < ApplicationController
-  def new
+  def create
     @bookmark = Bookmark.new
+    @bookmark.painting = Painting.find(params[:painting_id])
+    @bookmark.user = current_user
+    authorize @bookmark
+    if @bookmark.save
+      redirect_to painting_path(@painting)
+    end
   end
 
-  def create
+  def destroy
+    bookmark = Bookmark.find(params[:id])
+    @painting = bookmark.painting
+    bookmark.delete
+    redirect_to painting_path(@painting)
   end
 end
